ee02b17a13e9eb58f10468376ff11798d56b3783
Additya Popli <additya1998@gmail.com>
2018-05-01 11:36:00 +0530

    .

---
+++ Assignment2/build/CMakeFiles/graphics_asgn1.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/additya/IIIT/Graphics/Assignment2/src/audio.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/audio.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/ball.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/ball.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/boat.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/boat.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/cannon.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/cannon.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/color.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/color.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/helper.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/helper.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/input.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/input.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/island.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/island.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/main.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/main.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/man.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/man.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/monster.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/monster.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/nonedit.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/nonedit.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/other_handlers.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/other_handlers.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/power.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/power.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/rock.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/rock.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/timer.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/timer.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/trail.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/trail.cpp.o"
+  "/home/additya/IIIT/Graphics/Assignment2/src/water.cpp" "/home/additya/IIIT/Graphics/Assignment2/build/CMakeFiles/graphics_asgn1.dir/src/water.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Assignment2/build/CMakeFiles/graphics_asgn1.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphics_asgn1.dir/src/boat.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/audio.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/ball.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/island.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/rock.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/other_handlers.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/main.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/color.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/water.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/nonedit.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/timer.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/man.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/monster.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/helper.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/input.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/cannon.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/trail.cpp.o"
+  "CMakeFiles/graphics_asgn1.dir/src/power.cpp.o"
+  "graphics_asgn1.pdb"
+  "graphics_asgn1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graphics_asgn1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
